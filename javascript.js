48e66fa3427a295d2d2d87f8c1250a48b98d20ef
Sk Mainul <148805053+skm693@users.noreply.github.com>
2024-02-12 10:40:12 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function cl(x) {
                 break;
             }
         }
-        if(left==0){
+        if (left == 0) {
             document.getElementById('pop_up3').style.display = "flex";
             document.getElementById('rest').style.display = "none";
         }
@@ -30,8 +30,7 @@ function cl(x) {
                     break;
                 }
             }
-            if (ch == 1)
-                document.getElementById(zz).innerHTML = "X";
+
             let c1 = document.getElementById('b1').innerHTML;
             let c2 = document.getElementById('b2').innerHTML;
             let c3 = document.getElementById('b3').innerHTML;
@@ -41,14 +40,29 @@ function cl(x) {
             let c7 = document.getElementById('b7').innerHTML;
             let c8 = document.getElementById('b8').innerHTML;
             let c9 = document.getElementById('b9').innerHTML;
+            let winnar=0;
             if ((c1 == "O" && c2 == "O" && c3 == "O") || (c4 == "O" && c5 == "O" && c6 == "O") || (c7 == "O" && c8 == "O" && c9 == "O") || (c1 == "O" && c4 == "O" && c7 == "O") || (c2 == "O" && c5 == "O" && c8 == "O") || (c3 == "O" && c6 == "O" && c9 == "O") || (c1 == "O" && c5 == "O" && c9 == "O") || (c3 == "O" && c5 == "O" && c7 == "O")) {
                 document.getElementById('pop_up').style.display = "flex";
                 document.getElementById('rest').style.display = "none";
+                winnar=1;
             }
-            else if ((c1 == "X" && c2 == "X" && c3 == "X") || (c4 == "X" && c5 == "X" && c6 == "X") || (c7 == "X" && c8 == "X" && c9 == "X") || (c1 == "X" && c4 == "X" && c7 == "X") || (c2 == "X" && c5 == "X" && c8 == "X") || (c3 == "X" && c6 == "X" && c9 == "X") || (c1 == "X" && c5 == "X" && c9 == "X") || (c3 == "X" && c5 == "X" && c7 == "X")) {
+            if (ch == 1 && winnar==0) {
+                document.getElementById(zz).innerHTML = "X";
+                c1 = document.getElementById('b1').innerHTML;
+                c2 = document.getElementById('b2').innerHTML;
+                c3 = document.getElementById('b3').innerHTML;
+                c4 = document.getElementById('b4').innerHTML;
+                c5 = document.getElementById('b5').innerHTML;
+                c6 = document.getElementById('b6').innerHTML;
+                c7 = document.getElementById('b7').innerHTML;
+                c8 = document.getElementById('b8').innerHTML;
+                c9 = document.getElementById('b9').innerHTML;
+            }
+            if ((c1 == "X" && c2 == "X" && c3 == "X") || (c4 == "X" && c5 == "X" && c6 == "X") || (c7 == "X" && c8 == "X" && c9 == "X") || (c1 == "X" && c4 == "X" && c7 == "X") || (c2 == "X" && c5 == "X" && c8 == "X") || (c3 == "X" && c6 == "X" && c9 == "X") || (c1 == "X" && c5 == "X" && c9 == "X") || (c3 == "X" && c5 == "X" && c7 == "X")) {
                 document.getElementById('pop_up2').style.display = "flex";
                 document.getElementById('rest').style.display = "none";
             }
+
         }
     }
 }
